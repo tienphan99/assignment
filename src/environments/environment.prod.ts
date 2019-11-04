89bf0e3c00c778714de0ff00122001303757bae3
tienphan99 <phanvantien488@gmail.com>
2019-11-04 07:48:21 +0700

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCe8s77oaoCd7oCaAicKCL8MKjjFHrceAQ",
+    authDomain: "btlangular.firebaseapp.com",
+    databaseURL: "https://btlangular.firebaseio.com",
+    projectId: "btlangular",
+    storageBucket: "btlangular.appspot.com",
+    messagingSenderId: "401526717218",
+    appId: "1:401526717218:web:5196f6b1548081ecd4837b",
+    measurementId: "G-1LHYKP8R6X"
+  }
 };
